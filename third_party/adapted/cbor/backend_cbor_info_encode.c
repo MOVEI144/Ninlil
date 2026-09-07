e551bce2da79120bffc174ac87746ec1bd180e1d
Aero <daitsu115@gmail.com>
2026-09-07 23:57:00 +0900

    build: pin EDHOC and preserve audited compatibility adapters

---
+++ third_party/adapted/cbor/backend_cbor_info_encode.c
@@ -0,0 +1,58 @@
+/*
+ * Generated using zcbor version 0.8.1
+ * https://github.com/NordicSemiconductor/zcbor
+ * Generated with a --default-max-qty of 3
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <string.h>
+#include "zcbor_encode.h"
+#include "backend_cbor_info_encode.h"
+#include "zcbor_print.h"
+
+#if DEFAULT_MAX_QTY != 3
+#error "The type file was generated with a different default_max_qty than this file"
+#endif
+
+static bool encode_info(zcbor_state_t *state, const struct info *input);
+
+
+/* Exact callback types: no incompatible function-pointer casts. */
+static bool ninlil_decoder_encode_info(zcbor_state_t *state, void *data)
+{
+    return encode_info(state, data);
+}
+
+static bool encode_info(
+		zcbor_state_t *state, const struct info *input)
+{
+	zcbor_log("%s\r\n", __func__);
+
+	bool tmp_result = (((((zcbor_int32_encode(state, (&(*input).info_label))))
+	&& ((zcbor_bstr_encode(state, (&(*input).info_context))))
+	&& ((zcbor_uint32_encode(state, (&(*input).info_length)))))));
+
+	if (!tmp_result) {
+		zcbor_trace_file(state);
+		zcbor_log("%s error: %s\r\n", __func__, zcbor_error_str(zcbor_peek_error(state)));
+	} else {
+		zcbor_log("%s success\r\n", __func__);
+	}
+
+	return tmp_result;
+}
+
+
+
+int cbor_encode_info(
+		uint8_t *payload, size_t payload_len,
+		const struct info *input,
+		size_t *payload_len_out)
+{
+	zcbor_state_t states[2];
+
+	return zcbor_entry_function(payload, payload_len, (void *)input, payload_len_out, states,
+		ninlil_decoder_encode_info, sizeof(states) / sizeof(zcbor_state_t), 3);
+}
